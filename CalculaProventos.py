15073cc852bc137552726f3f305496d34d773e34
RPerobeli <rodrigo.costa2015@engenharia.ufjf.br>
2023-01-07 10:20:20 -0300

    Encapsulamento da leitura dos extratos

--- CalculaProventos.py
+++ CalculaProventos.py
@@ -1,7 +1,10 @@
 import pandas as pd
+import ReadExtracts as RE
+import os
 
-df = pd.read_csv('extrato-2022.12.01-2022.12.31.csv', sep = ';')
+os.system('cls' if os.name == 'nt' else 'clear')
 
+df = RE.LerExtratosCSV_Clear()
 print(df.columns)
 RendimentosTotais = df.query(f'Descrição.str.contains("RENDIMENTO") or Descrição.str.contains("DIVIDENDOS") or  Descrição.str.contains("JUROS S/CAPITAL")')
 # print(RendimentosTotais)
