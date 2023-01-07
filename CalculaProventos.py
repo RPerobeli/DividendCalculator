f5687ae9bf1467c31bce939eefc2ee50306354cc
RPerobeli <rodrigo.costa2015@engenharia.ufjf.br>
2023-01-07 11:01:53 -0300

    insercao de filtro por data

--- CalculaProventos.py
+++ CalculaProventos.py
@@ -1,9 +1,25 @@
 import pandas as pd
 import ReadExtracts as RE
 import os
+import datetime as dt
 
 os.system('cls' if os.name == 'nt' else 'clear')
+
+mesInicio = 1
+anoInicio = 2022
+mesFim = 1
+anoFim = 2023
+
+
+dataInicio = dt.datetime(anoInicio, mesInicio, 1)
+dataFim = dt.datetime(anoFim, mesFim, 1)
+
 df = RE.LerExtratosCSV_Clear()
+df['Liquidação'] = pd.to_datetime(df['Liquidação'], format="%d/%m/%Y")
+
+filtroData = (df['Liquidação'] > dataInicio) & (df['Liquidação'] <= dataFim)
+df = df.loc[filtroData]
+
 RendimentosTotais = df.query(f'Descrição.str.contains("RENDIMENTO") or Descrição.str.contains("DIVIDENDOS") or  Descrição.str.contains("JUROS S/CAPITAL")')
 Valores = RendimentosTotais['Valor'].replace(',', '.',regex=True)
 Valores = Valores.astype(float)
