75fb8da0dfbe6ac4ea239ca6c3d34e9b3dd20aae
RPerobeli <rodrigo.costa2015@engenharia.ufjf.br>
2023-01-07 10:37:03 -0300

    teste

--- CalculaProventos.py
+++ CalculaProventos.py
@@ -3,7 +3,6 @@ import ReadExtracts as RE
 import os
 
 os.system('cls' if os.name == 'nt' else 'clear')
-
 df = RE.LerExtratosCSV_Clear()
 RendimentosTotais = df.query(f'Descrição.str.contains("RENDIMENTO") or Descrição.str.contains("DIVIDENDOS") or  Descrição.str.contains("JUROS S/CAPITAL")')
 Valores = RendimentosTotais['Valor'].replace(',', '.',regex=True)
