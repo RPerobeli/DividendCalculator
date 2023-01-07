ced565441f8cd2870c5bc4b44d35bafd5cd3b4b2
RPerobeli <rodrigo.costa2015@engenharia.ufjf.br>
2023-01-07 10:21:54 -0300

    alteracoes para melhor display

--- CalculaProventos.py
+++ CalculaProventos.py
@@ -5,9 +5,8 @@ import os
 os.system('cls' if os.name == 'nt' else 'clear')
 
 df = RE.LerExtratosCSV_Clear()
-print(df.columns)
 RendimentosTotais = df.query(f'Descrição.str.contains("RENDIMENTO") or Descrição.str.contains("DIVIDENDOS") or  Descrição.str.contains("JUROS S/CAPITAL")')
 # print(RendimentosTotais)
 Valores = RendimentosTotais['Valor'].replace(',', '.',regex=True)
 Valores = Valores.astype(float)
-print(Valores.sum())
+print(f'Total de dividendos do período: {Valores.sum()}')
